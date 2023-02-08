cd9192291cc1edc4bce6bf0e1f809046ecc421bb
TreleaniFederico <treleani.federico@gmail.com>
2023-02-08 16:39:35 +0100

    add milestone 3

--- src/org/lessons/java/db/java/mysql/Main.java
+++ src/org/lessons/java/db/java/mysql/Main.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
@@ -13,6 +14,10 @@ public class Main {
 		String user = "root";
 		String password = "root";
 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Filtra i risultati. Inserisci la parola da ricercare: ");
+		String userInput = sc.nextLine();
+
 		try (Connection con = DriverManager.getConnection(url, user, password)) {
 			System.out.println("Connessione OK");
 
@@ -20,9 +25,11 @@ public class Main {
 			String sql = "SELECT c.name  AS country_name,c.country_id AS country_id,r.name AS region_name, cn.name  AS continent_name  \n"
 					+ "FROM dump_nations.countries c\n" + "INNER JOIN dump_nations.regions r\n"
 					+ "ON c.region_id  = r.region_id \n" + "INNER JOIN dump_nations.continents cn \n"
-					+ "ON r.continent_id  = cn.continent_id \n" + "ORDER BY c.name ";
+					+ "ON r.continent_id  = cn.continent_id \n" + "WHERE c.name LIKE ? \n" + "ORDER BY c.name ";
 
 			try (PreparedStatement ps = con.prepareStatement(sql)) {
+
+				ps.setString(1, "%" + userInput + "%");
 
 				try (ResultSet rs = ps.executeQuery()) {
 					while (rs.next()) {
